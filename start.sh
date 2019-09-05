720d7b90c6934e344acccf2ab5cd81b29ee35361
TomomasaTakatori <takatori@cloud-ace.jp>
2019-09-05 16:12:20 +0900

    複数のimageを作成するように

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 docker pull maven:3.6.1-jdk-8-slim
-docker build -t gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test3-STEP1 -t gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test3-STEP2
+docker build -t gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test3-STEP1 -f Dockerfile_step1 .
+docker build -t gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test3-STEP2 -f Dockerfile_step2 .
 docker push gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test3-STEP1
 docker push gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test3-STEP2
