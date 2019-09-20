fffefd9f37725b639f2f61b49193065f7b06f3bd
TomomasaTakatori <takatori@cloud-ace.jp>
2019-09-20 17:33:00 +0900

    [fix]change tag
    tagの数字を4から5に変更。

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export DEVSHELL_PROJECT_ID=ca-highbird-test
 docker pull maven:3.6.1-jdk-8-slim
-docker build -t gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test4-STEP1 -f Dockerfile_step1 .
-docker build -t gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test4-STEP2 -f Dockerfile_step2 .
-docker push gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test4-STEP1
-docker push gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test4-STEP2
+docker build --no-cache=true -t gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test4-STEP1 -f Dockerfile_step1 .
+docker build --no-cache=true -t gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test4-STEP2 -f Dockerfile_step2 .
+docker push gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test5-STEP1
+docker push gcr.io/${DEVSHELL_PROJECT_ID}/kfmp:test5-STEP2
